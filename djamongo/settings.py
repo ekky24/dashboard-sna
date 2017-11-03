b7704c95c384a5cad1693d1675a5df26dd2cdac6
Andri Fachrur Rozie <4r53n1c@gmail.com>
2017-11-03 14:31:47 +0700

    Add Additional Settings for Remote Connection

--- djamongo/settings.py
+++ djamongo/settings.py
@@ -25,26 +25,26 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
-    'dashboard',
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'users',
-    'widget_tweaks',
-    'postagger'
+	'dashboard',
+	'django.contrib.admin',
+	'django.contrib.auth',
+	'django.contrib.contenttypes',
+	'django.contrib.sessions',
+	'django.contrib.messages',
+	'django.contrib.staticfiles',
+	'users',
+	'widget_tweaks',
+	'postagger'
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware'
+	'django.middleware.security.SecurityMiddleware',
+	'django.contrib.sessions.middleware.SessionMiddleware',
+	'django.middleware.common.CommonMiddleware',
+	'django.middleware.csrf.CsrfViewMiddleware',
+	'django.contrib.auth.middleware.AuthenticationMiddleware',
+	'django.contrib.messages.middleware.MessageMiddleware',
+	'django.middleware.clickjacking.XFrameOptionsMiddleware'
 ]
 
 ROOT_URLCONF = 'djamongo.urls'
@@ -54,20 +54,20 @@ TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 
 TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [TEMPLATE_DIR],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-                'django.template.context_processors.static',
-            ],
-        },
-    },
+	{
+		'BACKEND': 'django.template.backends.django.DjangoTemplates',
+		'DIRS': [TEMPLATE_DIR],
+		'APP_DIRS': True,
+		'OPTIONS': {
+			'context_processors': [
+				'django.template.context_processors.debug',
+				'django.template.context_processors.request',
+				'django.contrib.auth.context_processors.auth',
+				'django.contrib.messages.context_processors.messages',
+				'django.template.context_processors.static',
+			],
+		},
+	},
 ]
 
 WSGI_APPLICATION = 'djamongo.wsgi.application'
@@ -77,10 +77,11 @@ WSGI_APPLICATION = 'djamongo.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': env('DB_ENGINE'),
-        'NAME': env('DB_NAME'),
-    }
+	'default': {
+		'ENGINE': env('DB_ENGINE'),
+		'NAME': env('DB_NAME'),
+		'HOST': env('DB_HOST')
+	}
 }
 
 
@@ -88,18 +89,18 @@ DATABASES = {
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
+	{
+		'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+	},
+	{
+		'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+	},
+	{
+		'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+	},
+	{
+		'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+	},
 ]
 
 
@@ -136,29 +137,29 @@ LOGOUT_REDIRECT_URL = '/dashboard/login/'
 if DEBUG:
    INTERNAL_IPS = ('127.0.0.1', 'localhost',)
    MIDDLEWARE += (
-       'debug_toolbar.middleware.DebugToolbarMiddleware',
+	   'debug_toolbar.middleware.DebugToolbarMiddleware',
    )
 
    INSTALLED_APPS += (
-       'debug_toolbar',
+	   'debug_toolbar',
    )
 
    DEBUG_TOOLBAR_PANELS = [
-       'debug_toolbar.panels.versions.VersionsPanel',
-       'debug_toolbar.panels.timer.TimerPanel',
-       'debug_toolbar.panels.settings.SettingsPanel',
-       'debug_toolbar.panels.headers.HeadersPanel',
-       'debug_toolbar.panels.request.RequestPanel',
-       'debug_toolbar.panels.sql.SQLPanel',
-       'debug_toolbar.panels.staticfiles.StaticFilesPanel',
-       'debug_toolbar.panels.templates.TemplatesPanel',
-       'debug_toolbar.panels.cache.CachePanel',
-       'debug_toolbar.panels.signals.SignalsPanel',
-       'debug_toolbar.panels.logging.LoggingPanel',
-       'debug_toolbar.panels.redirects.RedirectsPanel',
+	   'debug_toolbar.panels.versions.VersionsPanel',
+	   'debug_toolbar.panels.timer.TimerPanel',
+	   'debug_toolbar.panels.settings.SettingsPanel',
+	   'debug_toolbar.panels.headers.HeadersPanel',
+	   'debug_toolbar.panels.request.RequestPanel',
+	   'debug_toolbar.panels.sql.SQLPanel',
+	   'debug_toolbar.panels.staticfiles.StaticFilesPanel',
+	   'debug_toolbar.panels.templates.TemplatesPanel',
+	   'debug_toolbar.panels.cache.CachePanel',
+	   'debug_toolbar.panels.signals.SignalsPanel',
+	   'debug_toolbar.panels.logging.LoggingPanel',
+	   'debug_toolbar.panels.redirects.RedirectsPanel',
    ]
 
    DEBUG_TOOLBAR_CONFIG = {
-       'INTERCEPT_REDIRECTS': False,
+	   'INTERCEPT_REDIRECTS': False,
    }
    
